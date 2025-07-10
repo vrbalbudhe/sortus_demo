498ce556be5dba062d6d2909ac5fd62f2aa6243c
vrbalbudhe <vrbalbudhe@gmail.com>
2025-07-10 11:59:07 +0530

    Cookies Error Is Solved

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -146,8 +146,8 @@ export const login = async (req, res, next) => {
 
     await res.cookie('token', token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      secure: true,
+      sameSite: 'none',
       maxAge: 2 * 24 * 60 * 60 * 1000,
     });
 
